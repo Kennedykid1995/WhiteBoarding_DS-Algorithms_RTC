ef7fc17207701ccd7ad42f0f36538ee161358ed4
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2019-01-01 23:55:12 -0600

    Data Structures

--- problems.js
+++ problems.js
@@ -1058,4 +1058,83 @@ linearSearch([1,2,34,5], 5) //returns 3.
     //Big O
     //O(nk) - time /O(n + k) -space
 
+//Data structures 
+    //singly linked lists
+       //stores a piece  of data - next
+       //refrence to next node - next
+
+       class Node{
+           constructor(val){
+               this.val = val; 
+               this.next = null
+           }
+       }
+       class singlyLinkedList{
+           constructor(){
+            this.head = null; 
+            this.tail = null;
+            this.length = 0; 
+           }
+           push(val){
+               let newNode = new Node(val);
+               if(!this.head){
+                   this.head = newNode; 
+                   this.tail = this.head; 
+               }else{
+                   this.tail.next = newNode; 
+                   this.tail = newNode; 
+               }
+               this.length ++; 
+               return this; 
+           }
+        //    traverse(){
+        //        let current = this.head; 
+        //        while(current){
+        //            console.log(current.val); 
+        //            current = current.next; 
+        //        }
+        //    }
+           pop(){
+               //if there is no nodes in the list return undefines
+               //loop through the list until you reach the tail
+               //set the next property of the 2nd to last node to be null
+               //set the tail to be the 2nd to last node
+               //decrement the length of the list by 1
+               //return the value of the node removed
+               if(!this.head) return undefined; 
+               let current = this.head;
+               let newTail = current; 
+               while(current.next){
+                   newTail = current; //always lagging behind
+                   current = current.next; 
+               }
+               this.tail = newTail;
+               this.tail.next = null; 
+               this.length--;
+
+               if(this.length === 0){
+                   this.head = null; 
+                   this.tail = null; 
+               }
+               return current 
+           }
+           shift(){
+               if(!this.head) return undefined;
+               let currentHead = this.head;
+               this.head = currentHead.next;
+               this.length--; 
+               if(this.length === 0){
+                   this.head = null;
+                   this.tail = null; 
+               }
+               return currentHead; 
+           }
+       }
     
+       let list = new singlyLinkedList(val)
+       list.push("hello")
+       list.push("goodbye")
+       list.push("bye bye")
+
+
+
