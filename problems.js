406efadc5051a150ea8a54d0190cda6a9156191c
keith Kennedy <keithkennedy@e0-ac-cb-99-52-a2.valpo.edu>
2019-01-03 14:27:18 -0600

    Doubly linked list finished

--- problems.js
+++ problems.js
@@ -1303,47 +1303,180 @@ class doublyLinkedList {
     this.length++;
     return this;
   }
-  pop(){
-      //if there is no tail, head, return undefined
-      //store the current tail in a variable to return later
-      //if the length is -1 set the head and tail to null
-      //update the tail to be the previous node
-      //set the newTails next to null
-      //decrement the length 
-      //return the value removed
-      if(!this.head) return undefined;
-      let poppedNode = this.tail; 
-      if(this.length == 1){
-        this.head = null;
-        this.tail = null;
-      }else{
-        this.tail = poppedNode.prev;
-        this.tail.next = null;
-        poppedNode.prev = null;
-      }
-      this.length--;
-      return poppedNode;
+  pop() {
+    //if there is no tail, head, return undefined
+    //store the current tail in a variable to return later
+    //if the length is -1 set the head and tail to null
+    //update the tail to be the previous node
+    //set the newTails next to null
+    //decrement the length
+    //return the value removed
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
   }
-  shift(){
-      //removing from the beginning
-      //check if the length is 0 or no head and return undefined
-      //store the current head in a var
-      //if the length is one set the head and tail to null
-      //update the head to be the next of the old head
-      //set the heads prev property to null
-      //set the old head's next to null
-      //decrement the length
-      //return old head
-      if(!this.head) return undefined; 
-      let oldHead = this.head;
-      if(this.length === 1){
-          this.head = null;
-          this.tail = null
-      }
+  shift() {
+    //removing from the beginning
+    //check if the length is 0 or no head and return undefined
+    //store the current head in a var
+    //if the length is one set the head and tail to null
+    //update the head to be the next of the old head
+    //set the heads prev property to null
+    //set the old head's next to null
+    //decrement the length
+    //return old head
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
       this.head = oldHead.next;
       this.head.prev = null;
       oldHead.next = null;
-      this.length--;
-      return oldHead;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    //adding a node to the beginning
+    //create a new node with the val passed in
+    //if the length is 0
+    //set the head and tail to be the new node
+    //otherwise
+    //set the prev property on the head of the list to be
+    //the new node
+    //set the property on the new node to be the head property
+    //update the head to be the new node.
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
+    }
+  }
+  get(index) {
+    //accessing a node in a doubly linked list by position
+    //if the index is less than 0 or greater or equal to the
+    //length return null
+    //if the length is less than or equal to the length of the list
+    //loop through the list starting from the head and loop
+    //towards the middle
+    //return the node once found
+    //if the index is greater than half the length of the list
+    //loop through the list starting from the tail and loop towards
+    //the middle
+    //return the node once it is found
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      let count = this.length - 1;
+      let current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+      return current;
+    }
+  }
+  set(index, val) {
+    //replacing the value of a node to the doubly linked list
+    //create a var which is the result of the get method
+    //at the index passed to the function
+    //if the get method returns a valid node set the value
+    //of that node to be the value passed to the function
+    //return true
+    //otherwise return false
+    let foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    //adding a node in a doubly linked list by a certain position
+    //if the index is less than zero or greater than or equal to the length return false
+    //if the index is 0 unshift
+    //if the index is the same as the length push
+    //use get to access the index -1;
+    //set the next and prev properties on the corrent nodes to link everything
+    //increment the length
+    //return true
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+    let beforeNode = this.get(index - 1);
+    let newNode = new Node(val);
+    let afterNode = beforeNode.next;
+
+    beforeNode.next = newNode; //connections
+    newNode.prev = beforeNode; //5(bN)<-> 7(aN) insert 6(nN)
+    newNode.next = afterNode; //5(bN) <-> 6(nN) <-> 7(aN)
+    afterNode.prev = newNode;
+
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    //if the index is less than zero or greater than or equal to the length return undefined
+    //if the index is 0 shift
+    //if the index is the same as the length -1 pop
+    //use the get method to retrieve the item to be removed
+    //update the next and prev properties to remove the found node from the list
+    //set next and prev to null on the found node
+    //decrement the length
+    //return the removed node
+    if (index < 0 || index >= this.lenght) return false;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let removedNode = this.get(index);
+    let beforeNode = removedNode.prev;
+    let afterNode = removedNode.next;
+
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+    //removedNode.prev.next = removedNode.next
+    //removedNode.next.prev = removedNode.prev;
+
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return removedNode;
   }
 }
+
+//comparing singly vs doubly
+    //big O
+    //All Doubly
+    //insertion O(1)
+    //removal O(1)
+    //searching O(n)
+    //access - O(n)
+
+//big O of a singly linked list
+    //insertion - O(1)
+    //removal - it depends O(1) or O(n)
+    //searching - O(n)
+    //access - O(n)
